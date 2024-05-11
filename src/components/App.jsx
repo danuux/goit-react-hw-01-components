6aaea4ebce65da58be2d4b8c194710f3880aec7a
danuux <danut_laurentiu98@yahoo.com>
2024-05-11 20:38:22 +0300

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from './user.json';
 import data from './data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 export const App = () => {
   return (
@@ -13,9 +17,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,39 @@
+import styles from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+function FriendList({ friends }) {
+    return (
+      <div className={styles.friends}>
+        <ul className={styles['friend-list']}>
+          {friends.map(friend => (
+            <li key={friend.id} className={styles.items}>
+              <span
+                className={`${styles.status} ${
+                  friend.isOnline ? styles.online : styles.offline
+                }`}
+              ></span>
+              <img
+                className={styles.avatar}
+                src={friend.avatar}
+                alt={friend.name}
+              />
+              <span className={styles.name}>{friend.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
+export default FriendList;
